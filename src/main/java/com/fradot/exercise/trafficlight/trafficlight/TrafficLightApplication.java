7714a2a585a1c0547f91cb76deb98270df49c770
fradot <francesco.tucceri@gmail.com>
2020-07-01 17:07:42 +0100

    Base project skeleton

---
+++ src/main/java/com/fradot/exercise/trafficlight/trafficlight/TrafficLightApplication.java
@@ -0,0 +1,13 @@
+package com.fradot.exercise.trafficlight.trafficlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fradot/exercise/trafficlight/trafficlight/TrafficLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fradot.exercise.trafficlight.trafficlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

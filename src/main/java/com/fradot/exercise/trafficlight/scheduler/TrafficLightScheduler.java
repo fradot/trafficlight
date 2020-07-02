70591263d203117e842484bb30a1b96a48acd55d
fradot <francesco.tucceri@gmail.com>
2020-07-02 15:01:48 +0100

    Java docs formatting and unit test update.

--- src/main/java/com/fradot/exercise/trafficlight/scheduler/TrafficLightScheduler.java
+++ src/main/java/com/fradot/exercise/trafficlight/scheduler/TrafficLightScheduler.java
@@ -13,9 +13,9 @@ import org.springframework.statemachine.StateMachine;
 
 /**
  * This class defined a custom {@link org.springframework.scheduling.TaskScheduler} by providing a custom implementation
- * of the {@link ThreadPoolTaskScheduler} to the {@link ScheduledTaskRegistrar}. By using this custom configuration it's
- * possible to register a custom {@link org.springframework.scheduling.Trigger} (see {@link TrafficLightTrigger}) and
- * control the scheduled execution interval dynamically.
+ * of the {@link ThreadPoolTaskScheduler} to the {@link ScheduledTaskRegistrar}.
+ * By using this custom configuration it's possible to register a custom {@link org.springframework.scheduling.Trigger} (see {@link TrafficLightTrigger})
+ * and control the scheduled execution interval dynamically.
  */
 @Configuration
 @EnableScheduling
@@ -25,19 +25,32 @@ public class TrafficLightScheduler implements SchedulingConfigurer {
     private TrafficLightTrigger trafficLightTrigger;
 
     @Autowired
-    public TrafficLightScheduler(TrafficLightTrigger trafficLightTrigger, StateMachine<TrafficLightState, TrafficLightTransition> stateMachine) {
+    public TrafficLightScheduler(TrafficLightTrigger trafficLightTrigger,
+                                 StateMachine<TrafficLightState, TrafficLightTransition> stateMachine) {
         this.stateMachine = stateMachine;
         this.trafficLightTrigger = trafficLightTrigger;
     }
 
 
+    /**
+     * Configure {@link ScheduledTaskRegistrar} in order to execute my custom {@link TrafficLightTrigger}.
+     *
+     * @param taskRegistrar the {@link ScheduledTaskRegistrar} to be customized
+     */
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         taskRegistrar.setTaskScheduler(threadPoolTaskScheduler());
-        taskRegistrar.addTriggerTask(() -> stateMachine.sendEvent(TrafficLightTransition.TRANSITION), trafficLightTrigger);
+        taskRegistrar.addTriggerTask(() -> stateMachine.sendEvent(TrafficLightTransition.TRANSITION),
+                trafficLightTrigger);
     }
 
-    @Bean(destroyMethod="shutdown")
+    /**
+     * Provides a custom implementation of the {@link ThreadPoolTaskScheduler}.
+     * Defining the destroyMethod to shutdown in order to terminate the scheduler.
+     *
+     * @return a custom implementation of the {@link ThreadPoolTaskScheduler}
+     */
+    @Bean(destroyMethod = "shutdown")
     public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
         ThreadPoolTaskScheduler threadPoolTaskScheduler
                 = new ThreadPoolTaskScheduler();
--- src/test/java/com/fradot/exercise/trafficlight/statemachine/TrafficLightStateMachineConfigTest.java
+++ src/test/java/com/fradot/exercise/trafficlight/statemachine/TrafficLightStateMachineConfigTest.java
@@ -1,7 +1,5 @@
 package com.fradot.exercise.trafficlight.statemachine;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +8,9 @@ import org.springframework.statemachine.test.StateMachineTestPlan;
 import org.springframework.statemachine.test.StateMachineTestPlanBuilder;
 import org.springframework.test.context.event.annotation.AfterTestClass;
 
-
+/**
+ * Test class for the state machine configuration {@link TrafficLightStateMachineConfig} class.
+ */
 @SpringBootTest
 public class TrafficLightStateMachineConfigTest {
 
@@ -27,29 +27,33 @@ public class TrafficLightStateMachineConfigTest {
     public void itShouldHaveRedAsTheInitialState() throws Exception {
         StateMachineTestPlan<TrafficLightState, TrafficLightTransition> plan =
                 StateMachineTestPlanBuilder.<TrafficLightState, TrafficLightTransition>builder()
-                        .stateMachine(stateMachine)
-                        .step()
+                    .stateMachine(stateMachine)
+                    .step()
                         .expectStates(TrafficLightState.RED)
-                        .and()
-                        .build();
+                    .and()
+                    .build();
         plan.test();
     }
 
-    @DisplayName("It should transition from RED to GREEN and then to ORANGE.")
+    @DisplayName("It should transition from RED to GREEN and then to ORANGE and back to RED.")
     @Test
     public void itShouldTransitionToGreenThenToOrange() throws Exception {
         StateMachineTestPlan<TrafficLightState, TrafficLightTransition> plan =
                 StateMachineTestPlanBuilder.<TrafficLightState, TrafficLightTransition>builder()
-                        .stateMachine(stateMachine)
-                        .step()
+                    .stateMachine(stateMachine)
+                    .step()
                         .sendEvent(TrafficLightTransition.TRANSITION)
                         .expectStates(TrafficLightState.GREEN)
-                        .and()
-                        .step()
+                    .and()
+                    .step()
                         .sendEvent(TrafficLightTransition.TRANSITION)
                         .expectState(TrafficLightState.ORANGE)
-                        .and()
-                        .build();
+                    .and()
+                    .step()
+                        .sendEvent(TrafficLightTransition.TRANSITION)
+                        .expectState(TrafficLightState.RED)
+                    .and()
+                    .build();
 
         plan.test();
     }
